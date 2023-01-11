76412c54519cd62db0fa93b6b2e98a6a62f18dc5
Dontavious Green <dontavious.o.green@gmail.com>
2023-01-11 05:20:53 -0600

    Initial

---
+++ index.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name = "viewport" content = "width=device-width">
+        <title>Dontavious Green Resume</title>
+    </head>
+    <body>
+        <h1>Dontavious Green</h1>
+            <p></p>
+    </body>
+</html>
